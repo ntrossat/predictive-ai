0e8d64b8015c88bba4b40b0401ed271f03dcb614
Nicolas Trossat <nicolas.trossat@gmail.com>
2024-10-06 16:05:11 +0200

    Remove sqlite

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import sqlite3
-from sqlalchemy import create_engine
 
 from app import predict
 
-db = sqlite3.connect("predictive_ai.db")
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 
 
 def run():
