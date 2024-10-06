0fa7f5809b5def12c823234b3e2768ef9ae71129
Nicolas Trossat <nicolas.trossat@gmail.com>
2024-10-06 09:15:45 +0200

    Introduce SQLite in main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import click
+import sqlite3
+from sqlalchemy import create_engine
+
+db = sqlite3.connect("predictive_ai.db")
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 
 
 @click.command()
-def cli():
-    """Example script."""
-    click.echo("Hello World!")
+@click.option("--name", help="Your name", default="World")
+def run(name):
+    print(f"Hello, {name}!")
