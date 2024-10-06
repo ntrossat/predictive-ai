a0b1e7f1df9bdb3670a729c2126baeb761ebf403
Nicolas Trossat <nicolas.trossat@gmail.com>
2024-10-06 16:02:29 +0200

    Remove Click from main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import click
 import sqlite3
 from sqlalchemy import create_engine
 
@@ -8,10 +7,7 @@ db = sqlite3.connect("predictive_ai.db")
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 
 
-@click.command()
-@click.option("--name", help="Your name", default="World")
-def run(name):
-    print(f"Hello, {name}!")
+def run():
     predict.run()
 
 
